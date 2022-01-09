9877e61a6aa2ca8c905ee063e3ebe54962a72f8f
Eder Lima <devilsouls666@gmail.com>
2022-01-09 16:02:52 -0300

    feat: add absolute paths to cli application

--- src/core/command_line.rs
+++ src/core/command_line.rs
@@ -6,16 +6,23 @@ use std::path::Path;
 pub fn execute(arguments: Vec<String>) {
     let filename = &arguments[0];
     let tablename = filename.replace(".csv", "");
-    let file_path = Path::new(&filename)
+    let source_file_path = Path::new(&filename)
         .canonicalize()
         .expect("Couldn't get path for source file");
 
-    let contents = fs::read_to_string(file_path).expect("Couldn't read from source file. :(");
+    let output_file = format!("{}.sql", tablename);
+    let output_file_path = Path::new(&output_file);
+
+    let contents =
+        fs::read_to_string(source_file_path).expect("Couldn't read from source file. :(");
 
     let table_schema = get_table_schema(&contents);
     let create_table = get_create_table_query(&tablename, &table_schema, &contents);
     let insert_data = get_insert_data_query(&tablename, &table_schema, &contents);
 
-    fs::write("data.sql", format!("{}\n\n{}", create_table, insert_data))
-        .expect("Couldn't write to file! :(");
+    fs::write(
+        output_file_path,
+        format!("{}\n\n{}", create_table, insert_data),
+    )
+    .expect("Couldn't write to file! :(");
 }
